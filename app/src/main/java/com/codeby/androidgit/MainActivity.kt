74c5d5ea4f3099d497487c02584a98a5e33f336a
Preslock <example@you.com>
2023-12-07 19:11:10 +0300

    new variable_1

--- app/src/main/java/com/codeby/androidgit/MainActivity.kt
+++ app/src/main/java/com/codeby/androidgit/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var x : Int = 13
+    var x : Int = 33
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
